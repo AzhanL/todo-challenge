e48c2d0126a4eb44a72d838c04f4df241e48592c
azhan <azhanl@protonmail.com>
2020-12-15 03:53:43 -0500

    feat: create add and remove  actions and mutations for todos

--- src/state/todos.ts
+++ src/state/todos.ts
@@ -5,7 +5,8 @@ export interface TodosState {
   todos: Todo[];
 }
 
-type Todo = {
+export type Todo = {
+  id: number;
   title: string;
   description: string;
   dueDate: Date;
@@ -14,8 +15,24 @@ type Todo = {
 export const TodosStore: Module<TodosState, RootState> = {
   namespaced: true,
   state: {
-      todos: []
+    todos: [],
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_TODO(state, payload: Todo) {
+      state.todos.push(payload);
+    },
+    REMOVE_TODO(state, payload: number) {
+      const position = state.todos.findIndex((todo) => todo.id == payload);
+
+      state.todos.splice(position, 1);
+    },
+  },
+  actions: {
+    addTodo({commit}, payload: Todo) {
+      commit('ADD_TODO', payload)
+    },
+    removeTodo({commit}, id: number) {
+      commit('REMOVE_TODO', id)
+    }
+  },
 };
